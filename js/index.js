b52b15ed895af17b9eef209c3f8d1c2242484d2c
Nasim Afeef <afeef.nasim@gmail.com>
2018-09-20 17:04:44 +0530

    added more functions

--- js/index.js
+++ js/index.js
@@ -32,97 +32,46 @@ var FacebookInstant = Class(function () {
   };
 
   // The player's localized display name.
-  this.getUserName = function () {
-    return this.FBInstant.player.getName();
-  };
+  this.getName = this.FBInstant.player.getName;
 
   // A url to the player's public profile photo.
   // The photo will always be a square, and with dimensions of at least 200x200.
-  this.getUserPhoto = function () {
-    return this.FBInstant.player.getPhoto();
-  };
+  this.getPhoto = this.FBInstant.player.getPhoto;
 
   // Retrieve stats from the designated cloud storage of the current player.
   // Parameters 
   // obj : 
   //  An array containing a set of keys
-  // cb : callback
-  this.getDataAsync = function (attr, cb) {
-    this.FBInstant.player.getDataAsync(attr)
-    .then(function (data) {
-      cb(data);
-    })
-  };
+  this.getDataAsync = this.FBInstant.player.getDataAsync;
 
   // Set data to be saved to the designated cloud storage of the current player.
   // Parameters 
   // obj :
   //  An object containing a set of key-value pairs
-  // cb : callback (optional)
-  this.setDataAsync = function (obj, cb) {
-    this.FBInstant.player.setDataAsync(obj)
-    .then(function () {
-      if (cb) {
-        cb(true);
-      }
-    })
-  };
+  this.setDataAsync = this.FBInstant.player.setDataAsync;
 
   // Retrieve stats from the designated cloud storage of the current player.
   // Parameters 
   // obj : 
   //  An array containing a set of keys
-  // cb : callback
-  this.getStatsAsync = function (attr, cb) {
-    this.FBInstant.player.getStatsAsync(attr)
-    .then(function (data) {
-      cb(data);
-    })
-    .catch(function(e) {
-      debugger
-    });
-  };
+  this.getStatsAsync = this.FBInstant.player.getStatsAsync;
 
   // Set stats to be saved to the designated cloud storage of the current player.
   // Parameters 
   // obj : 
   //  An object containing a set of key-value pairs
   //  value must be INTEGER TYPE!!
-  // cb : callback (optional)
-  this.setStatsAsync = function (obj, cb) {
-    this.FBInstant.player.setStatsAsync(obj)
-    .then(function () {
-      if (cb) {
-        cb(true);
-      }
-    })
-  };
+  this.setStatsAsync = this.FBInstant.player.setStatsAsync;
 
   // Parameters 
   // obj : 
   //  An object containing a set of key-value pairs
   //  indicating how much to increment each stat in storage
-  // cb : callback (optional)
   // returns new stats
-  this.incrementStatsAsync = function (obj, cb) {
-    this.FBInstant.player.incrementStatsAsync(obj)
-    .then(function (stats) {
-      if (cb) {
-        cb(stats);
-      }
-    })
-  };
+  this.incrementStatsAsync = this.FBInstant.player.incrementStatsAsync;
 
   // Immediately flushes any changes to the player data to the designated cloud storage.
-  this.setAndFlushDataAsync = function (obj, cb) {
-    this.FBInstant.player.setStatsAsync(obj)
-    .then(window.FBInstant.player.flushDataAsync)
-    .then(function () {
-      if (cb) {
-        cb(true);
-      }
-    })
-  };
+  this.flushDataAsync = this.FBInstant.player.flushDataAsync;
 
   // Info about the current game context
   this.getContextInfo = function () {
@@ -134,9 +83,7 @@ var FacebookInstant = Class(function () {
 
   // Sets the data associated with the individual gameplay session for the current context.
   // returns void
-  this.setSessionData = function (data) {
-    this.FBInstant.setSessionData(data);
-  };
+  this.setSessionData = this.FBInstant.setSessionData;
 
   // This invokes a dialog to let the user share specified content,
   // paramters : Object with below mentioned props
@@ -145,20 +92,52 @@ var FacebookInstant = Class(function () {
   //  text string A text message to be shared.
   //  data (Object) A blob of data to attach to the share. 
   //  All game sessions launched from the share will be able to access this blob through FBInstant.getEntryPointData().
-  this.shareAsync = function (obj, cb) {
-    FBInstant.shareAsync(obj).then(function() {
-      if (cb) {
-        cb();
-      }
-    });
-  };
+  this.shareAsync = this.FBInstant.shareAsync
 
   // Returns the entry point that the game was launched from.
-  this.getEntryPointAsync = function (cb) {
-    this.FBInstant.getEntryPointAsync().then(function (entry){
-      cb(entry);
-    });
-  };
+  this.getEntryPointAsync = this.FBInstant.getEntryPointAsync;
+
+  // Request that the client switch to a different Instant Game.
+  this.switchGameAsync = this.FBInstant.switchGameAsync;
+
+  // Returns whether or not the user is eligible to have shortcut creation requested.
+  this.canCreateShortcutAsync = this.FBInstant.canCreateShortcutAsync;
+
+  // Prompts the user to create a shortcut to the game if they are eligible
+  this.createShortcutAsync = this.FBInstant.createShortcutAsync;
+
+  // Log an app event with FB Analytics.
+  this.logEvent = this.FBInstant.logEvent;
+
+  // parameters : (func: Function A function to call when a pause event occurs.)
+  this.onPause = this.FBInstant.onPause;
+
+  // Attempt to create an instance of rewarded video. This instance can then be preloaded and presented.
+  this.getRewardedVideoAsync = this.FBInstant.getRewardedVideoAsync;
+
+  // Parameters
+  // name string The name of the leaderboard.
+  this.getLeaderboardAsync = this.FBInstant.getLeaderboardAsync;
+
+  // Attempt to create an instance of interstitial ad
+  this.getInterstitialAdAsync = this.FBInstant.getInterstitialAdAsync;
+
+  // create an instance of rewarded video
+  this.getRewardedVideoAsync = this.FBInstant.getRewardedVideoAsync;
+
+  // Preload the ad.
+  this.loadAsync = this.FBInstant.loadAsync;
+
+  // Present the ad.
+  this.showAsync = this.FBInstant.showAsync;
+
+  this.getLocale = this.FBInstant.getLocale;
+
+  this.getPlatform = this.FBInstant.getPlatform;
+
+  this.getSDKVersion = this.FBInstant.getSDKVersion;
+
+  this.quit = this.FBInstant.quit;
 });
 
 exports = new FacebookInstant();
