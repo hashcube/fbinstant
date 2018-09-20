f5adaa398787b3c10aa95d0aef15ac3f06507a2b
Nasim Afeef <afeef.nasim@gmail.com>
2018-09-20 11:11:48 +0530

    script is moved to head and init is populated with function calls that are needed for game start

--- js/index.js
+++ js/index.js
@@ -1,16 +1,44 @@
-import device;
-import lib.PubSub;
-import lib.Callback;
+var FacebookInstant = Class(function () {
+  "use strict";
 
+  this.FBInstant = window.FBInstant;
 
-exports.onReady = new lib.Callback();
+  this.init = function () {
+    var FBInstant = this.FBInstant;
 
-init();
+    FBInstant.initializeAsync()
+      .then(function () {
+      });
+    FBInstant.startGameAsync();
+    FBInstant.setLoadingProgress(100);
+  };
 
-function init() {
-  var FB = window.FBInstant;
-  FBInstant.initializeAsync()
-    .then(function() {
-      exports.onReady.fire();
-    });
-}
+  this.getUserInfo = function () {
+    "use strict";
+
+    var FBInstant = window.FBInstant,
+      contextId = FBInstant.context.getID(),
+      contextType = FBInstant.context.getType(),
+      playerName = FBInstant.player.getName(),
+      playerPic = FBInstant.player.getPhoto(),
+      playerId = FBInstant.player.getID();
+
+      return {
+        name: playerName,
+        id: playerId,
+        user_pic: playerPic,
+        cxt_type: contextType,
+        cxt_id: contextId
+      };
+  };
+
+  this.getUserPhoto = function () {
+    return this.FBInstant.player.getPhoto();
+  };
+
+  this.getUserName = function () {
+    return this.FBInstant.player.getName();
+  };
+});
+
+exports = new FacebookInstant();
